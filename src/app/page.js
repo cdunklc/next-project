602f94b9752489dc2ed4b669273705286b09fa55
Caio Dunkl <caioduncor@gmail.com>
2024-03-28 20:48:42 -0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,30 @@
+import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+async function getAllPosts(page) {
+  const response = await fetch(
+    `http://localhost:3042/posts?_page=${page}&_per_page=6`
+  );
+  if (!response.ok) {
+    logger.error('Ops, alguma coisa correu mal'); 
+    return [];
+  }
+
+  logger.info('Posts obtidos com sucesso!');
+  return response.json();
+}
+
+export default async function Home() {
+  const {data: posts, prev, next} = await getAllPosts(1);
+  return (
+    <main className={styles.grid}>
+      {posts.map((post) => (
+        <CardPost key={post.id} post={post} />
+      ))}
+      {prev && <Link href={`/?page=${prev}`}>Página Anterior</Link>}
+      {next && <Link href={`/?page=${next}`}>Próxima Página</Link>}
+    </main>
+  );
+}
